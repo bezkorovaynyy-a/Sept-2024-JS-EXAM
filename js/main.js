d8b4df95eb5ccc5e4e5c4f531402733afc0de316
Oleksiy Mykolayovych <uaspace7@gmail.com>
2024-12-06 15:08:14 +0200

    Правка бордера підсвічювання поля

---
+++ js/main.js
@@ -0,0 +1,123 @@
+const input = document.getElementById('input');// Поле введення даних
+const list = document.getElementById('list');// Блок для формування списку
+
+function checkInputValue(inputValue) {// Функція перевірки введених даних на їх коректність
+    let result = false;
+    const regex = /^[A-Za-zА-Яа-яґҐЁёІіЇїЄє0-9 =]+$/;// Пара значень має містити тільки букви, цифри та знак 1 '='. Тяжко з цим(((
+    const isValid = regex.test(inputValue);
+    if (isValid) {
+        const arrInputValue = inputValue.split('=');
+        if (arrInputValue.length === 2 && arrInputValue[0] !== '' && arrInputValue[1] !== '') {// Введене значення коректне
+            result = true;
+        }
+        else if (arrInputValue.length > 2) {// Присутньо більше одного знака '='
+            addErrorMsg(`The separator '=' must be 1`);
+        }
+        else if (!inputValue.includes('=')) {// Пара не розділена знаком '='
+            addErrorMsg(`'Pare must be separated by the 1 '=' sign`);
+        }
+        else {// 'Name' або 'Value' пусті
+            addErrorMsg(`'Name' and 'Value' must not be empty`);
+        }
+    } else {// Введене значення містить недопустимі символи
+        addErrorMsg(`Only letters, numbers and 1 '=' sign`);
+    }
+    return result;
+}
+
+function addErrorMsg(errorText) {// Функція створення повідомлення про помилку
+    const errorMsg = document.createElement('div');
+    errorMsg.classList.add('error-msg');
+    errorMsg.textContent = errorText;
+    document.getElementsByClassName('main-container')[0].appendChild(errorMsg);
+}
+
+function checkErrorMsg() {// Функція перевірки повідомлення про помилку та видалення якщо дані вірні
+    const errorMsgElement = document.getElementsByClassName('error-msg')[0];
+    if (errorMsgElement) {
+        errorMsgElement.remove();
+        input.classList.remove('is-invalid');
+        list.classList.remove('is-invalid');
+    }
+}
+
+function addElementToList(content) {// Функція додавання елементу в блок списку
+    const newElementList = document.createElement('p');
+    newElementList.textContent = content;
+    list.prepend(newElementList);
+    newElementList.onclick = function () {// Функція вибору елементів для видалення зі списку
+        this.classList.contains('mark') ? this.classList.remove('mark') : this.classList.add('mark');
+    }
+}
+
+const btnAdd = document.getElementById('btnAdd');// Подія на кнопку додавання пари значень в список
+btnAdd.onclick = function () {
+    checkErrorMsg();
+    let inputValue = input.value.trim();
+    if (inputValue === '') {
+        input.classList.add('is-invalid');
+        addErrorMsg('This field is required');
+    } else {
+        let correctInputValue = checkInputValue(inputValue);
+        if (correctInputValue === true) {
+            let arrNameValue = inputValue.split('=');
+            inputValue = `${arrNameValue[0].trim()}=${arrNameValue[1].trim()}`;
+            addElementToList(inputValue);
+            input.value = '';
+            input.focus();
+        }
+    }
+}
+
+const btnDel = document.getElementById('btnDel');// Подія на кнопку видалення елементів зі списку
+btnDel.onclick = function () {
+    checkErrorMsg();
+    const listElements = document.querySelectorAll('p');
+    if (listElements.length === 0) {
+        list.classList.add('is-invalid');
+        addErrorMsg('The list contains no elements');
+    } else {
+        let counterSelectedElements = 0;
+        for (let i = 0; i < listElements.length; i++) {
+            if (listElements[i].classList.contains('mark')) {
+                listElements[i].remove();
+                counterSelectedElements++;
+            }
+        }
+        if (counterSelectedElements === 0) {
+            list.classList.add('is-invalid');
+            addErrorMsg('Mark (click) item(s) to be deleted');
+        }
+    }
+}
+
+const sortButtons = document.getElementsByName('sort');// Подія на кнопки сортування
+for (let i = 0; i < sortButtons.length; i++) {
+    sortButtons[i].onclick = function () {
+        checkErrorMsg();
+        const listElements = [...document.querySelectorAll('p')];
+        if (listElements.length === 0) {
+            addErrorMsg('There are no items in the list to sort');
+        } else if (listElements.length === 1) {
+            addErrorMsg('There is only 1 element in the list');
+        } else {
+            const directionSort = +this.value;
+            listElements.sort((a, b) => {
+                if (a.innerText.split('=')[directionSort] > b.innerText.split('=')[directionSort]) {
+                    return 1;
+                }
+                if (a.innerText.split('=')[directionSort] < b.innerText.split('=')[directionSort]) {
+                    return -1;
+                }
+                if (a.innerText.split('=')[directionSort] === b.innerText.split('=')[directionSort]) {
+                    return 0;
+                }
+            });
+            listElements.reverse();
+            list.innerHTML = '';
+            for (let i = 0; i < listElements.length; i++) {
+                addElementToList(listElements[i].innerText);
+            }
+        }
+    };
+}
